ff22dc0057014520c73d07aaa24828c75edb332b
Prabhjyot Singh Gill <Prabhjyotg@gmail.com>
2021-11-17 14:23:33 -0500

    Added @material-ui/core

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import "../views/Header.css";
+import MenuIcon from '@mui/icons-material/Menu';
+import {IconButton} from "@material-ui/core";
 
 function Header(){
     return <div className="header">
-        <h2>Header</h2>
+        <div className="header__left">
+            <IconButton>
+                <MenuIcon></MenuIcon>
+            </IconButton>
+        </div>
+        <div className="header__middle"></div>
+        <div className="header__right"></div>
+
     </div>
 }
 
