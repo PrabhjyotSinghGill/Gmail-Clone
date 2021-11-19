89e5667c59f4288a120ec8aa5d5fee9cbf345cc0
Prabhjyot Singh Gill <Prabhjyotg@gmail.com>
2021-11-19 10:37:11 -0500

    Sidebar Component Complete

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,6 +7,11 @@ import {Button} from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import InboxIcon from "@material-ui/icons/Inbox";
 import StarIcon from "@material-ui/icons/Star";
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import LabelImportantIcon from '@mui/icons-material/LabelImportant';
+import NearMeIcon from '@mui/icons-material/NearMe';
+import NoteIcon from '@mui/icons-material/Note';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function Sidebar(){
     return<div className="sidebar">
@@ -15,10 +20,15 @@ function Sidebar(){
         className="sidebar__compose">Compose
         </Button>
 
-        <SidebarOption Icon={InboxIcon} title="Inbox" number={54}>
+        <SidebarOption Icon={InboxIcon} title="Inbox" number={54} selected={true}>
         </SidebarOption>
         <SidebarOption Icon={StarIcon} title="Starred" number={54}>
         </SidebarOption>
+        <SidebarOption Icon={AccessTimeIcon} title="Snoozed" number={54}></SidebarOption>
+        <SidebarOption Icon={LabelImportantIcon} title="Important" number={54}></SidebarOption>
+        <SidebarOption Icon={NearMeIcon} title="Sent" number={54}></SidebarOption>
+        <SidebarOption Icon={NoteIcon} title="Drafts" number={54}></SidebarOption>
+        <SidebarOption Icon={ExpandMoreIcon} title="More" number={54}></SidebarOption>
     </div>
 }
 
--- src/components/SidebarOption.jsx
+++ src/components/SidebarOption.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "../views/SidebarOption.css";
 
-function SidebarOption({Icon, title, number}){
-    return <div className="sidebar__option">
+function SidebarOption({Icon, title, number, selected}){
+    return <div className={`sidebar__option ${selected && 'sidebarOption--active'}`}>
         <Icon></Icon>
         <h3>{title}</h3>
         <p>{number}</p>
