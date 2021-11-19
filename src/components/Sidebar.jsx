457a9c40dfdf7978cbb6f5706e95d51dec435960
Prabhjyot Singh Gill <Prabhjyotg@gmail.com>
2021-11-19 13:55:58 -0500

    Sidebar Component Footer

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import SidebarOption from "../components/SidebarOption.jsx";
 //CSS 
 import "../views/Sidebar.css";
 //Material UI imports
-import {Button} from "@material-ui/core";
+import {Button, IconButton} from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import InboxIcon from "@material-ui/icons/Inbox";
 import StarIcon from "@material-ui/icons/Star";
@@ -12,6 +12,9 @@ import LabelImportantIcon from '@mui/icons-material/LabelImportant';
 import NearMeIcon from '@mui/icons-material/NearMe';
 import NoteIcon from '@mui/icons-material/Note';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import PersonIcon from '@mui/icons-material/Person';
+import DuoIcon from '@mui/icons-material/Duo';
+import PhoneIcon from '@mui/icons-material/Phone';
 
 function Sidebar(){
     return<div className="sidebar">
@@ -29,6 +32,19 @@ function Sidebar(){
         <SidebarOption Icon={NearMeIcon} title="Sent" number={54}></SidebarOption>
         <SidebarOption Icon={NoteIcon} title="Drafts" number={54}></SidebarOption>
         <SidebarOption Icon={ExpandMoreIcon} title="More" number={54}></SidebarOption>
+        <div className="sidebar__footer">
+            <div className="sidebar__footerIcons">
+                <IconButton>
+                    <PersonIcon></PersonIcon>
+                </IconButton>
+                <IconButton>
+                    <DuoIcon></DuoIcon>
+                </IconButton>
+                <IconButton>
+                    <PhoneIcon></PhoneIcon>
+                </IconButton>
+            </div>
+        </div>
     </div>
 }
 
