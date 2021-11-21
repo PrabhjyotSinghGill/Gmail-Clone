c9a9b55a46d3418a334fe4b041b35d0480d00567
Prabhjyot Singh <prabhjyotg@gmail.com>
2021-11-20 20:57:35 -0500

    Updated Components Mail.jsx, EmailList.jsx

--- src/components/EmailList.jsx
+++ src/components/EmailList.jsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import {Checkbox, IconButton} from "@material-ui/core"; 
 import "../views/EmailList.css";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import RedoIcon from '@mui/icons-material/Redo';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import KeyboardHideIcon from '@mui/icons-material/KeyboardHide';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 function EmailList(){
-    return <div className="emailList"></div>
+    return <div className="emailList">
+        <div className="emailList__settings">
+            <div className="emailList__setttingsLeft">
+                <Checkbox></Checkbox>
+                <IconButton>
+                    <ArrowDropDownIcon></ArrowDropDownIcon>
+                </IconButton>
+                <IconButton>
+                    <RedoIcon></RedoIcon>
+                </IconButton>
+                <IconButton>
+                    <MoreVertIcon></MoreVertIcon>
+                </IconButton>
+            </div>
+            <div className="emailList__settingsRight">
+                <IconButton>
+                    <ChevronLeftIcon></ChevronLeftIcon>
+                </IconButton>
+                <IconButton>
+                    <ChevronRightIcon></ChevronRightIcon>
+                </IconButton>
+                <IconButton>
+                    <KeyboardHideIcon></KeyboardHideIcon>
+                </IconButton>
+                <IconButton>
+                    <SettingsIcon></SettingsIcon>
+                </IconButton>
+            </div>
+            <div className="emailList__sections">
+                
+            </div>
+        </div>
+    </div>
 }
 export default EmailList 
