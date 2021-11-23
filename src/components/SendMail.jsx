e76c426adb3cf791c51618cd32bca1fa397e735d
Prabhjyot Singh Gill <Prabhjyotg@gmail.com>
2021-11-23 16:28:00 -0500

    Working on SendMail PopUp CSS

--- src/components/SendMail.jsx
+++ src/components/SendMail.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import "../views/SendMail.css";
+import CloseIcon from '@mui/icons-material/Close';
+import { Button } from "@mui/material";
 
 function SendMail(){
-    return <div className="sendMail"></div>;
+    return <div className="sendMail">
+        <div className="sendMail__header">
+            <h3>New Message</h3>
+            <CloseIcon className="sendMail__close"></CloseIcon>
+        </div>
+        <form>
+        <input type="text"></input>
+        <input type="text"></input>
+        <input type="text"></input>
+            <div className="sendMail__options">
+                <Button>Send</Button>
+            </div>
+        </form>
+    </div>;
 }
 export default SendMail
